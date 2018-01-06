a23e8be880996a5391d5797966f992e65e2cfe2b
stawary <15800916230@126.com>
2018-01-06 08:04:57 +0800

    Delete 可视化决策树.py

--- 可视化决策树.py
+++
@@ -1,8 +0,0 @@
-from sklearn.datasets import load_iris #导入莺尾花数据集
-iris=load_iris()
-print (iris.feature_names) #显示特征名
-print (iris.target_names)#显示目标种类莺尾花名称
-print (iris.data[0])
-print (iris.target[0])
-for i in range(len(iris.target)):
-    print ("Example %d: lable %s, festures %s" % (i,iris.target[i],iris.data[i]))#打印出所有莺尾花数据
